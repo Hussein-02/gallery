d5e11b36121bcbd0c751b283a97e87d8694ef0fd
Hussein-02 <hussein02ab@gmail.com>
2025-03-12 22:28:11 +0200

    finished delete api

---
+++ gallery-server/api/v1/deletePhoto.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . '../../models/Photo.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+$photo_id = $data['photo_id'];
+
+Photo::delete($photo_id);
+
+echo json_encode([
+    'status' => 'success',
+    'message' => 'Photo deleted successfully'
+]);
--- gallery-server/api/v1/updatePhoto.php
+++ gallery-server/api/v1/updatePhoto.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../../models/Photo.php';
+require __DIR__ . '../../models/Photo.php';
 
 
 $data = json_decode(file_get_contents('php://input'), true);
