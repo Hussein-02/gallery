a5c31a24767eff1ab8af67e6918f9fbac4ee5b86
Hussein-02 <hussein02ab@gmail.com>
2025-03-13 13:14:09 +0200

    finished signup page

---
+++ gallery-client/src/pages/Signup.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "../css/styles.css";
+
+const Signup = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost/article/article-server/api/v1/signup.php",
+        {
+          fullname: fullname,
+          email: email,
+          password: password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.data.success) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+
+        navigate("/home");
+      } else {
+        console.log(response.data.message);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="logo">GALLERY</h1>
+      <div className="login-section">
+        <h1>Sign Up</h1>
+        <form className="login-form" id="signupForm" onSubmit={handleSubmit}>
+          <div className="login-input">
+            <label htmlFor="fullname">Full Name</label>
+            <input
+              type="text"
+              id="fullname"
+              name="fullname"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
+            />
+          </div>
+
+          <div className="login-input">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="login-input">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <input type="submit" value="Sign Up" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
