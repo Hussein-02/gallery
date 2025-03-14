3ba5bc8545524aa9380c4e32ca8f758e1341c9a7
Hussein-02 <hussein02ab@gmail.com>
2025-03-14 09:47:06 +0200

    initialized photo page

--- gallery-client/src/routers/router.jsx
+++ gallery-client/src/routers/router.jsx
@@ -4,6 +4,7 @@ import App from "../App";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
 import Home from "../pages/Home";
+import Photo from "../pages/Photo";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: "/home", // Login route for '/'
         element: <Home />,
+      },
+      {
+        path: "/photo", // Login route for '/'
+        element: <Photo />,
       },
     ],
   },
